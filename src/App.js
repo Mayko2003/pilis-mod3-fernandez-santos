4d13bc635d930758f50a01a6cd867e0173efcdc6
Mayko2003 <maykox34f@gmail.com>
2022-10-07 11:09:06 -0300

    chore: add routing and init layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useContext, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { getWeather } from "./api/weatherAPI";
+import { UbicacionContext } from "./contexts/UbicacionContext";
+import { Navigation } from "./routes/Navigation/Navigation";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const { ubicaciones, setUbicaciones } = useContext(UbicacionContext)
+
+    useEffect(() => {
+
+        getWeather(-24.19457, -65.29712).then((data) => {
+            const nueva = {
+                name: "San Salvador de Jujuy",
+                lat: -24.19457,
+                lon: -65.29712,
+                temp: data.current_weather.temperature,
+                windspeed: data.current_weather.wind_speed,
+            }
+            setUbicaciones([...ubicaciones, nueva])
+        }).catch((error) => {
+            console.log(error)
+        })
+
+        getWeather(-24.7859, -65.41166).then((data) => {
+            const nueva = {
+                name: "Salta",
+                lat: -24.19457,
+                lon: -65.29712,
+                temp: data.current_weather.temperature,
+                windspeed: data.current_weather.wind_speed,
+            }
+            setUbicaciones([...ubicaciones, nueva])
+        }).catch((error) => {
+            console.log(error)
+        })
+
+        return () => {
+
+        }
+    }, [ ubicaciones, setUbicaciones ])
+
+
+    return (
+        <div >
+            <Routes>
+                <Route path='/' element={<Navigation/>}>
+                    
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
