b441f2031dd82c19381a48ec7f06e073c60a858b
Mayko2003 <maykox34f@gmail.com>
2022-10-14 14:33:29 -0300

    chore: add login

--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -1,11 +1,18 @@
 import { Link, Outlet } from "react-router-dom"
+import { useContext } from 'react'
+import { UserContext } from '../../contexts/UserContext'
 
 export const Navigation = () => {
+
+    const { setUser, user } = useContext(UserContext)
+
+    const logout = () => {
+        localStorage.removeItem('user')
+        setUser(null)
+    }
+
     return (
         <>
-        <head>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"></link>
-        </head>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <span className="nav-brand text-light">Wheater</span>
@@ -13,20 +20,32 @@ export const Navigation = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100">
                             <li className="nav-item">
                                 <Link className="nav-link" to='/'>Home</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to='/ubicacion/create'>Crear Ubicacion</Link>
+                            <li className="nav-item ms-lg-auto">
+                                <Link className="nav-link" to={user ? "/ubicacion/create" : "/login"}>Crear Ubicacion</Link>
                             </li>
+                            {
+                                user ?
+                                    (
+                                        <li className="nav-item">
+                                            <a href="." className="nav-link" onClick={logout}>Cerrar Sesion</a>
+                                        </li>
+                                    ) :
+                                    (
+                                        <li className="nav-item">
+                                            <Link className="nav-link" to='/login'>Iniciar Sesion</Link>
+                                        </li>
+                                    )
+                            }
+
                         </ul>
                     </div>
                 </div>
-            </nav>
-
+            </nav >
             <Outlet />
         </>
-
     )
 }
